fc0b7b128bddf3dcc0d4f84414e69d174daf59d8
QuinSDev <quindev97@gmail.com>
2023-10-26 22:40:42 -0500

    Sebastian_Quintero se crea el proyecto Mercado-Quincho.

---
+++ src/main/java/com/mercado/quincho/MercadoQuinchoApplication.java
@@ -0,0 +1,13 @@
+package com.mercado.quincho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoQuinchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoQuinchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercado/quincho/QuinchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercado.quincho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuinchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
